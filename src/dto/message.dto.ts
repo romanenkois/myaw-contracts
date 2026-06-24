476440934b2ffcaa3cc1ed11a15a6271a4459345
illya Romanenko <romanenkoisnot@gmail.com>
2026-06-24 17:30:28 +0300

    fix/messages dtos

--- src/dto/message.dto.ts
+++ src/dto/message.dto.ts
@@ -1,12 +1,14 @@
 export type MessageResponseDto = TextMessageResponseDto;
 
 export type MessageTypeResponseDto = 'text';
+export type MessageStatusResponseDto = 'sending' | 'delivered' | 'send' | 'error';
 
 export interface MessageResponseBaseDto<T extends MessageTypeResponseDto> {
   id: string;
   chatId: string;
   senderId: string;
   timestamp: string;
+  status: MessageStatusResponseDto;
   type: T;
   content: Record<string, unknown>;
 }
